cf1b1e841777448a09fb82d2d794cd694e34404b
Rafiq Islam <mi21b@fsu.edu>
2024-04-28 02:15:52 -0400

    new

--- src/desgld/desgld_alg.py
+++ src/desgld/desgld_alg.py
@@ -1,32 +1,10 @@
+import math
+
 import numpy as np
 
 
 class DeSGLD:
-    """Decentralized Stochastic Gradient Langevin Diffuision Algorithm"""
-
-    """
-    This class implements the Decentralized Stochastic Grad
-    Langevin Diffuision Algorithm (DeSGLD) for both vanila and
-    extra convergence analysis
-    """
-
     def __init__(self, size_w, N, sigma, eta, T, dim, b, lam, x, y, w, hv):
-        """Construct of the DeSGLD class"""
-
-        """
-        :param size_w: the size of the network
-        :param N: size of the array in each iteration
-        :param sigma: variance of the noise
-        :param eta: the step size
-        :param T: the number of iterations
-        :param dim: the dimension of the input data
-        :param b: batch size
-        :param lam: the regularization parameter
-        :param x: the input data
-        :param y: the output data
-        :param w: the initial weights/network weights
-        :param hv: h values for the extra convergence analysis
-        """
         self.size_w = size_w
         self.N = N
         self.sigma = sigma
@@ -41,12 +19,6 @@ class DeSGLD:
         self.hv = hv
 
     def gradient_logreg(self, beta, x, y, dim, lam, b):
-        """Gradient of the loss function for logistic regression"""
-
-        """
-        :param beta: parameters of interest
-        :return: the gradient of the loss function
-        """
         f = np.zeros(dim)
         for i in range(b):
             h = 1 / (1 + np.exp(-np.dot(beta, x[i])))
@@ -54,115 +26,49 @@ class DeSGLD:
         f = f + np.dot(2 / lam, beta)
         return f
 
-    def gradient_linreg(self, beta, x, y, dim, lam, b):
-        """Gradient of the loss function for linear regression"""
-
-        """
-        :param beta: parameters of interest
-        :return: the gradient of the loss function
-        """
-        f = []
-        for i in range(dim):
-            f.append(0)
-        f = np.array(f)
-        randomList = np.random.randint(0, len(y) - 1, size=int(b))
-        for item in randomList:
-            f = f - np.dot((y[item] - np.dot(beta, x[item])), x[item])
-        f = f + np.dot(2 / lam, beta)
-        return f
-
     def vanila_desgld_logreg(self):
-        """Vanilla DeSGLD algorithm for logistic regression"""
-
-        """
-        :return: parameters of interest from the vanila DeSGLD algorithm
-        :param: history_all: Combined output from all the nodes
-        :param: beta_mean_all: Mean approximation from all the nodes
-        """
-
         # Initialization
-        beta = np.random.normal(
-            0, self.sigma, size=(self.N, self.size_w, self.dim)
-        )
-        history_all = np.zeros((self.T + 1, self.size_w, self.dim, self.N))
-        beta_mean_all = np.zeros((self.T + 1, self.dim, self.N))
+        beta = np.random.normal(0, self.sigma, (self.N, self.size_w, self.dim))
+        history_all = []
+        beta_mean_all = []
 
-        for i in range(self.size_w):
+        for _ in range(1):
+            history = np.empty((self.size_w, self.dim, self.N))
+            beta_mean = np.empty((self.dim, self.N))
+
+            for i in range(self.size_w):
+                for d in range(self.dim):
+                    history[i, d] = beta[:, i, d]
             for d in range(self.dim):
-                history_all[0, i, d] = beta[:, i, d].T
-                beta_mean_all[0, d] += history_all[0, i, d] / self.size_w
+                beta_mean[d] = np.mean(history[:, d], axis=0)
+            history_all.append(history)
+            beta_mean_all.append(beta_mean)
 
-        # Update the iterations
+        # Main loop (update)
         step = self.eta
-        for t in range(1, self.T + 1):
-            noise = np.random.normal(
-                0, self.sigma, size=(self.N, self.size_w, self.dim)
-            )
+        for t in range(self.T):
             for n in range(self.N):
                 for i in range(self.size_w):
-                    for d in range(self.dim):
-                        g = self.gradient_logreg(
-                            beta[n, i],
-                            self.x[i],
-                            self.y[i],
-                            self.dim,
-                            self.lam,
-                            self.b,
-                        )
-                        temp = np.sum(self.w[i] * beta[n], axis=0)
-                        beta[n, i] = (
-                            temp - step * g + np.sqrt(2 * step) * noise[n, i]
-                        )
+                    g = self.gradient_logreg(
+                        beta[n, i],
+                        self.x[i],
+                        self.y[i],
+                        self.dim,
+                        self.lam,
+                        self.b,
+                    )
+                    temp = 0
+                    for j in range(len(beta[n])):
+                        temp = temp + self.w[i, j] * beta[n, j]
+                    noise = np.random.normal(0, self.sigma, self.dim)
+                    beta[n, i] = temp - step * g + math.sqrt(2 * step) * noise
 
-            history_all[t] = beta.transpose(1, 2, 0)
-            beta_mean_all[t] = np.mean(history_all[t], axis=0)
-
-        return history_all, beta_mean_all
-
-    def vanila_desgld_linreg(self):
-        """Vanilla DeSGLD algorithm  for linear regression"""
-
-        """
-        :return: parameters of interest from the vanila DeSGLD algorithm
-        :param: history_all: Combined output from all the nodes
-        :param: beta_mean_all: Mean approximation from all the nodes
-        """
-
-        # Initialization
-        beta = np.random.normal(
-            0, self.sigma, size=(self.N, self.size_w, self.dim)
-        )
-        history_all = np.zeros((self.T + 1, self.size_w, self.dim, self.N))
-        beta_mean_all = np.zeros((self.T + 1, self.dim, self.N))
-
-        for i in range(self.size_w):
+            history = np.empty((self.size_w, self.dim, self.N))
+            beta_mean = np.empty((self.dim, self.N))
+            for i in range(self.size_w):
+                for d in range(self.dim):
+                    history[i, d] = beta[:, i, d]
             for d in range(self.dim):
-                history_all[0, i, d] = beta[:, i, d].T
-                beta_mean_all[0, d] += history_all[0, i, d] / self.size_w
-
-        # Update the iterations
-        step = self.eta
-        for t in range(1, self.T + 1):
-            noise = np.random.normal(
-                0, self.sigma, size=(self.N, self.size_w, self.dim)
-            )
-            for n in range(self.N):
-                for i in range(self.size_w):
-                    for d in range(self.dim):
-                        g = self.gradient_linreg(
-                            beta[n, i],
-                            self.x[i],
-                            self.y[i],
-                            self.dim,
-                            self.lam,
-                            self.b,
-                        )
-                        temp = np.sum(self.w[i] * beta[n], axis=0)
-                        beta[n, i] = (
-                            temp - step * g + np.sqrt(2 * step) * noise[n, i]
-                        )
-
-            history_all[t] = beta.transpose(1, 2, 0)
-            beta_mean_all[t] = np.mean(history_all[t], axis=0)
-
-        return history_all, beta_mean_all
+                beta_mean[d] = np.mean(history[:, d], axis=0)
+            history_all.append(history)
+            beta_mean_all.append(beta_mean)
