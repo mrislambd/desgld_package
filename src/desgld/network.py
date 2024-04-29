6fdeb4699f557b4568bda171c488c44b176d213f
Rafiq Islam <mi21b@fsu.edu>
2024-04-29 14:53:27 -0400

    network updated

--- src/desgld/network.py
+++ src/desgld/network.py
@@ -2,23 +2,40 @@ import numpy as np
 
 
 class NetworkArchitecture:
-    """Decentralized network architecture"""
+    """Decentralized network architecture
 
-    """
-    This class is used to generate different network architectures. Here we add
-    four different network architectures: fully connected, fully disconnected,
-    circular network and star network.
+    Description:
+        This class is used to generate different network
+        architectures. Here we add four different network architectures:
+        fully connected, fully disconnected, circular network and star network.
+    Args:
+        size_w (int): the size of the network
     """
 
     def __init__(self, size_w):
-        """
-        :param size_w: the size of the network
-        """
         self.size_w = size_w
 
     def fully_connected(self):
-        """
-        :return: a fully connected network
+        """Fully Connected Network
+
+        Description:
+            A fully connected network is a kind of network in which all nodes
+            are connected to all other nodes.
+
+        Returns:
+            w (float): a fully connected network matrix
+
+        Examples:
+            >>> net = NetworkArchitecture(size_w=6)
+            >>> w=net.fully_connected()
+            >>> print(w)
+            w=[[1/6,1/6,1/6,1/6,1/6,1/6],
+                [1/6,1/6,1/6,1/6,1/6,1/6],
+                [1/6,1/6,1/6,1/6,1/6,1/6],
+                [1/6,1/6,1/6,1/6,1/6,1/6],
+                [1/6,1/6,1/6,1/6,1/6,1/6],
+                [1/6,1/6,1/6,1/6,1/6,1/6]]
+
         """
         return np.array(
             [
@@ -28,8 +45,26 @@ class NetworkArchitecture:
         )
 
     def circular_network(self):
-        """
-        :return: a circular network matrix
+        """Circular Network
+
+        Description:
+            A circular network is a kind of network in which a particular
+            node is connected to its left and right nodes only.
+
+        Returns:
+            w (float): a circular network matrix
+
+        Examples:
+            >>> net = NetworkArchitecture(size_w=6)
+            >>> w=net.circular()
+            >>> print(w)
+            w=[[1/3,1/3,0,0,0,1/3],
+                [1/3,1/3,1/3,0,0,0],
+                [0,1/3,1/3,1/3,0,0],
+                [0,0,1/3,1/3,1/3,0],
+                [0,0,0,1/3,1/3,1/3],
+                [1/3,0,0,0,1/3,1/3]]
+
         """
         x = [[0] * self.size_w for _ in range(self.size_w)]
         for i in range(self.size_w):
@@ -41,8 +76,26 @@ class NetworkArchitecture:
         return np.array(x)
 
     def fully_disconnected(self):
-        """
-        :return: a fully disconnected network matrix
+        """Completely Disconnected Network
+
+        Description:
+            A completely disconnected network is a kind of network in which
+            all the nodes are disconnected from each other.
+
+        Returns:
+            w (float): a disconnected network matrix
+
+        Examples:
+            >>> net = NetworkArchitecture(size_w=6)
+            >>> w=net.circular()
+            >>> print(w)
+            w=[[1,0,0,0,0,0],
+                [0,1,0,0,0,0],
+                [0,0,1,0,0,0],
+                [0,0,0,1,0,0],
+                [0,0,0,0,1,0],
+                [0,0,0,0,0,1]]
+
         """
         x = [[0] * self.size_w for _ in range(self.size_w)]
         for i in range(self.size_w):
@@ -52,8 +105,28 @@ class NetworkArchitecture:
         return np.array(x)
 
     def star_network(self):
-        """
-        :return: a star network matrix
+        """Star-like Connected Network
+
+        Description:
+            A star-like network is a kind of network in which
+            there is a central node and all other nodes are
+            connected to the central node. However, the individual
+            nodes are not connected to each other.
+
+        Returns:
+            w (float): a star network matrix
+
+        Examples:
+            >>> net = NetworkArchitecture(size_w=6)
+            >>> w=net.star_network()
+            >>> print(w)
+            w=[[1,0,0,0,0,1],
+                [0,1,0,0,0,1],
+                [0,0,1,0,0,1],
+                [0,0,0,1,0,1],
+                [0,0,0,0,1,1],
+                [0,0,0,0,0,1]]
+
         """
         s = [
             [
