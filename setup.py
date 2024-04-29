46973ac22ba11ce363dacd3fb74dd1980dca82c6
Rafiq Islam <mi21b@fsu.edu>
2024-04-28 21:18:10 -0400

    setup file added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+requirements = [
+    "numpy",
+    "scipy",
+]
+
+requirements_dev = ["black", "isort", "flake8", "pre-commit"]
+
+setup(
+    name="desgld",
+    version="0.1.0",
+    description="Package for decentralized stochastic gradient descent",
+    url="https://github.com/mrislambd/desgld_package.git",
+    author="Rafiq Islam",
+    packages=["desgld"],
+    package_dir={"": "src"},
+    install_requires=requirements,
+    extras_require={"dev": requirements_dev},
+)
