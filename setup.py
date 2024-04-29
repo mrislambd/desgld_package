4d58c9d5de4e70b7d7095d020acb39aea2177e84
Rafiq Islam <mi21b@fsu.edu>
2024-04-28 22:13:04 -0400

    documentation

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from setuptools.command.build_py import build_py as build_py_orig
+from setuptools.command.build_sphinx import build_sphinx
 
 requirements = [
     "numpy",
@@ -6,6 +8,13 @@ requirements = [
 ]
 
 requirements_dev = ["black", "isort", "flake8", "pre-commit"]
+
+
+class build_py(build_py_orig):
+    def run(self):
+        self.run_command("build_sphinx")
+        build_py_orig.run(self)
+
 
 setup(
     name="desgld",
@@ -17,4 +26,14 @@ setup(
     package_dir={"": "src"},
     install_requires=requirements,
     extras_require={"dev": requirements_dev},
+    cmdclass={
+        "build_py": build_py,
+        "build_sphinx": build_sphinx,
+    },
+    command_options={
+        "build_sphinx": {
+            "source_dir": ("setup.py", "docs/source"),
+            "build_dir": ("setup.py", "docs/html_build"),
+        },
+    },
 )
